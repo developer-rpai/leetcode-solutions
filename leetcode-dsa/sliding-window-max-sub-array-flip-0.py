eab4c106422bd18d1199c9d1b030d8a0bca4f2a3
developer-rpai <pairakesh10@gmail.com>
2024-12-02 20:22:28 -0600

    updates

---
+++ leetcode-dsa/sliding-window-max-sub-array-flip-0.py
@@ -0,0 +1,19 @@
+def find_length(s):
+    # curr is the current number of zeros in the window
+    left = curr = ans = 0 
+    for right in range(len(s)):
+        if s[right] == "0":
+            curr += 1
+        while curr > 1:
+            if s[left] == "0":
+                curr -= 1
+            left += 1
+        ans = max(ans, right - left + 1)
+    
+    return ans
+
+
+
+
+s = "11101100001111"
+print(find_length(s))
